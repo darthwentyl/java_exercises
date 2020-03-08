073e64703ddf104850319c364f8a95bac0d9b2c3
Michal Wendland <wendland.michal@gmail.com>
2020-03-08 17:43:13 +0100

    Final solution for point in triangle problem

--- PointInTriangle/src/triangle/Triangle.java
+++ PointInTriangle/src/triangle/Triangle.java
@@ -29,22 +29,50 @@ class Point {
 }
 
 public class Triangle {
-    private Point X;
-    private Point Y;
-    private Point Z;
+    private Point A;
+    private Point B;
+    private Point C;
 
-    Triangle(Point X_, Point Y_, Point Z_) {
-        X = X_;
-        Y = Y_;
-        Z = Z_;
+    Triangle(Point A_, Point B_, Point C_) {
+        A = A_;
+        B = B_;
+        C = C_;
+    }
+
+    // The area is calculated by Heron formula:
+    // https://en.wikipedia.org/wiki/Heron%27s_formula
+    public double calcArea() {
+        double a = A.dist(B);
+        double b = B.dist(C);
+        double c = C.dist(A);
+        double s = (a + b + c) / 2;
+        
+        double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+        
+        // rounded to three decimal places
+        area *= 1000;
+        area = Math.round(area);
+        area /= 1000;
+        return area;
     }
 
     public String toString() {
-        return "X" + X + "; Y" + Y + "; Z" + Z + ";";
+        return "A" + A + "; B" + B + "; C" + C + ";";
     }
-    
+
     public boolean isInTriangle(Point p) {
-        return false;
+        double thisArea = this.calcArea();
+
+        Triangle D1 = new Triangle(A, B, p);
+        double d1 = D1.calcArea();
+
+        Triangle D2 = new Triangle(A, p, C);
+        double d2 = D2.calcArea();
+
+        Triangle D3 = new Triangle(p, B, C);
+        double d3 = D3.calcArea();
+
+        return (thisArea - d1 - d2 - d3) == 0;
     }
 }
 
